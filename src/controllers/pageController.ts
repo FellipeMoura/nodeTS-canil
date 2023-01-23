5d3136f7c8b27e533428f3d2a6c32fd28e259c68
FellipeMoura <fellipevictormoura@gmail.com>
2023-01-23 09:00:28 -0300

    att

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 
 export const home = (req:Request, res:Response) =>{
 
-    // res.render('pages/page')
+    res.render('pages/page')
 }
 
 export const dogs = (req:Request, res:Response) =>{
@@ -17,5 +17,5 @@ export const cats = (req:Request, res:Response) =>{
 
 export const fishes = (req:Request, res:Response) =>{
 
-    // res.render('pages/page')
+    res.render('pages/page')
 }
