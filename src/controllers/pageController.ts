fc6283cd911d0c819aa271285cfd2a8c614a9923
FellipeMoura <fellipevictormoura@gmail.com>
2023-01-23 12:09:52 -0300

    att

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,46 @@
 import { Request, Response } from 'express'
+import {cMenu} from '../helpers/createMenu'
 
 export const home = (req:Request, res:Response) =>{
 
-    res.render('pages/page')
+    res.render('pages/page',{
+        menu: cMenu('all'), 
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    })
 }
 
 export const dogs = (req:Request, res:Response) =>{
 
-    // res.render('pages/page')
+     res.render('pages/page',{
+        menu: cMenu('dog'),
+        banner: {
+            title: 'Dogs',
+            background: 'banner_dog.jpg'
+        }
+     })
 }
 
 export const cats = (req:Request, res:Response) =>{
 
-    // res.render('pages/page')
+    res.render('pages/page',{
+        menu: cMenu('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+     })
 }
 
 export const fishes = (req:Request, res:Response) =>{
 
-    res.render('pages/page')
+    res.render('pages/page',{
+        menu: cMenu('fish'),
+        banner: {
+            title: 'Fishes',
+            background: 'banner_fish.jpg'
+        }
+     })
 }
