2cff38e99fdb3d0cd8e7b8888cdfcd4efe51935a
FellipeMoura <fellipevictormoura@gmail.com>
2023-01-24 11:53:02 -0300

    Projeto finalizado

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,46 +1,56 @@
 import { Request, Response } from 'express'
 import {cMenu} from '../helpers/createMenu'
+import { Pet } from '../models/pet'
+
 
 export const home = (req:Request, res:Response) =>{
+    let list = Pet.getAll()
 
     res.render('pages/page',{
         menu: cMenu('all'), 
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
-        }
+        },
+        list
     })
 }
 
 export const dogs = (req:Request, res:Response) =>{
+    let list = Pet.getFromType('dog')
 
      res.render('pages/page',{
         menu: cMenu('dog'),
         banner: {
             title: 'Dogs',
             background: 'banner_dog.jpg'
-        }
+        }, 
+        list
      })
 }
 
 export const cats = (req:Request, res:Response) =>{
+    let list = Pet.getFromType('cat')
 
     res.render('pages/page',{
         menu: cMenu('cat'),
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
-        }
+        },
+        list
      })
 }
 
 export const fishes = (req:Request, res:Response) =>{
+    let list = Pet.getFromType('fish')
 
     res.render('pages/page',{
         menu: cMenu('fish'),
         banner: {
             title: 'Fishes',
             background: 'banner_fish.jpg'
-        }
+        },
+        list
      })
 }
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,19 @@
 import{Request,Response} from 'express'
+import{Pet} from '../models/pet'
+import {cMenu} from '../helpers/createMenu'
 
 export const search = (req:Request, res:Response) =>{
+    let query: string =req.query.q as string
 
-    // res.render('pages/search')
+    if(!query){
+        res.redirect('/')
+        return
+    }
+
+    let list = Pet.getFromName(query)
+    res.render('pages/page', {
+        menu: cMenu(''),
+        list, query
+    })
+    
 }
